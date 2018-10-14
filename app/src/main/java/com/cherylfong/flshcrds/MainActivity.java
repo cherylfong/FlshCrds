44ad395d4756dbe296b8e0897e350fc0c2cdb9cc
cherylfong <42821602+cherylfong@users.noreply.github.com>
2018-10-14 01:05:14 -0700

    add logic to hide/show mulitple choice answers and eye icon

--- app/src/main/java/com/cherylfong/flshcrds/MainActivity.java
+++ app/src/main/java/com/cherylfong/flshcrds/MainActivity.java
@@ -1,12 +1,17 @@
 package com.cherylfong.flshcrds;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean toggle = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +72,32 @@ public class MainActivity extends AppCompatActivity {
                         (getResources().getColor(R.color.colorCorrect, null));
             }
         });
+
+        findViewById(R.id.hide_icon).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                ImageView iv = findViewById(R.id.hide_icon);
+
+                if(toggle){
+                   iv.setImageResource(R.drawable.see_icon);
+                    findViewById(R.id.flashc_answer).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashc_answer2).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashc_answer3).setVisibility(View.VISIBLE);
+
+                   toggle = false;
+                } else {
+                    iv.setImageResource(R.drawable.hide_icon);
+                    findViewById(R.id.flashc_answer).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashc_answer2).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashc_answer3).setVisibility(View.INVISIBLE);
+                    toggle = true;
+                }
+
+
+            }
+        });
+
 
     }
 
