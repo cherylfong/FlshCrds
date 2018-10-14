eeacd48703bc2226e5198b61e95695d6bea1ecb0
cherylfong <42821602+cherylfong@users.noreply.github.com>
2018-10-14 00:40:02 -0700

    add color logic to right and wrong answers

--- app/src/main/java/com/cherylfong/flshcrds/MainActivity.java
+++ app/src/main/java/com/cherylfong/flshcrds/MainActivity.java
@@ -3,6 +3,7 @@ package com.cherylfong.flshcrds;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,26 +13,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.flashc_question).setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-
-                    findViewById(R.id.flashc_answer).setVisibility(View.VISIBLE);
-                    findViewById(R.id.flashc_question).setVisibility(View.INVISIBLE);
-                    findViewById(R.id.flashc_answer).setBackgroundColor
-                            (getResources().getColor(R.color.colorAccent, null));
-            }
-        });
 
         findViewById(R.id.flashc_answer).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
 
-                findViewById(R.id.flashc_answer).setVisibility(View.INVISIBLE);
-                findViewById(R.id.flashc_question).setVisibility(View.VISIBLE);
+                // highlight right answer
+                findViewById(R.id.flashc_answer).setBackgroundColor
+                        (getResources().getColor(R.color.colorCorrect, null));
+
+                // to give better contrast
+                TextView tv = findViewById(R.id.flashc_answer);
+                tv.setTextColor(getResources().getColor(R.color.colorYellow, null));
+
+                // show yay!
             }
         });
 
+        findViewById(R.id.flashc_answer2).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                // hightlight right answer
+                TextView tv = findViewById(R.id.flashc_answer2);
+                tv.setTextColor(getResources().getColor(R.color.colorWrong, null));
+
+                // to give better contrast
+                TextView tv1 = findViewById(R.id.flashc_answer);
+                tv1.setTextColor(getResources().getColor(R.color.colorYellow, null));
+
+
+                // highlight right answer
+                findViewById(R.id.flashc_answer).setBackgroundColor
+                        (getResources().getColor(R.color.colorCorrect, null));
+            }
+        });
+
+        findViewById(R.id.flashc_answer3).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                // highlight wrong answer
+                TextView tv = findViewById(R.id.flashc_answer3);
+                tv.setTextColor(getResources().getColor(R.color.colorWrong, null));
+
+                // to give better contrast
+                TextView tv1 = findViewById(R.id.flashc_answer);
+                tv1.setTextColor(getResources().getColor(R.color.colorYellow, null));
+
+
+                // highlight right answer
+                findViewById(R.id.flashc_answer).setBackgroundColor
+                        (getResources().getColor(R.color.colorCorrect, null));
+            }
+        });
 
     }
 
