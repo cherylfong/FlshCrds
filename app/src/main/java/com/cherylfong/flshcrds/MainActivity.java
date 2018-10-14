0f3b642fabb78154719a06ee18c3c599f908c69d
cherylfong <42821602+cherylfong@users.noreply.github.com>
2018-10-13 21:48:54 -0700

    Create logic to show and hide flash card answer

--- app/src/main/java/com/cherylfong/flshcrds/MainActivity.java
+++ app/src/main/java/com/cherylfong/flshcrds/MainActivity.java
@@ -2,6 +2,8 @@ package com.cherylfong.flshcrds;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashc_question).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                if(findViewById(R.id.flashc_answer).getVisibility() == View.VISIBLE)
+                {
+                    findViewById(R.id.flashc_answer).setVisibility(View.INVISIBLE);
+                } else {
+                    findViewById(R.id.flashc_answer).setVisibility(View.VISIBLE);
+                }
+
+            }
+        });
     }
+
+
+
 }
